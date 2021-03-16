7125b450c7f334d78fcc419fae6a72dcef943aac
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-03-16 09:59:01 +0800

    added asdawdad

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("hello");
-        System.out.println("hi");
+        System.out.println("sakdjwlkdjalwd");
         System.out.println("im asdawdaw boss");
         System.out.println("ajkshdajklwd u");
+
+
+        System.out.println("asdasdawda");
     }
 }
